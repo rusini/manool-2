63b1e947f5425729cbbf43df58dd352aca11aa45
rusini <rusini@manool.org>
2021-01-06 11:01:43 -0500

    Update 5

---
+++ main2.cc
@@ -0,0 +1,45 @@
+
+# include <cstdio>
+# include <set>
+# include <utility>
+# include <vector>
+
+# include "rusini.hh"
+
+struct it {
+   it() = default;
+   it(const it &) { std::puts("it(const it &)"); }
+   it(it &&) { std::puts("it(it &)"); }
+};
+
+struct it2 {
+   it2() = default;
+   it2(const it2 &) { std::puts("it(const it2 &)"); }
+   it2(it2 &&) { std::puts("it(it2 &)"); }
+   it2(const it &) { std::puts("it2(const it &)"); }
+   it2(it &&) { std::puts("it2(it &&)"); }
+};
+
+std::vector<int> f1() { return {1, 2, 3}; }
+const std::vector<int> f2() { return {4, 5, 6}; }
+rsn::lib::slice_ref<std::vector<int>::iterator> f3() { return {}; }
+rsn::lib::slice_ref<std::vector<int>::const_iterator> f4() { return {}; }
+const rsn::lib::slice_ref<std::vector<int>::iterator> f5() { return {}; }
+const rsn::lib::slice_ref<std::vector<int>::const_iterator> f6() { return {}; }
+
+int main() {
+   std::vector<int> a1{11, 12, 13, 21, 22, 33};
+   const std::vector<int> a2{11, 12, 13, 21, 22, 33};
+   rsn::lib::slice_ref<std::vector<int>::iterator> s1;
+   rsn::lib::slice_ref<std::vector<int>::const_iterator> s2;
+   const rsn::lib::slice_ref<std::vector<int>::iterator> s3 = s1;
+   const rsn::lib::slice_ref<std::vector<int>::const_iterator> s4 = s1;
+
+   s2 = s4;
+
+   //int x[]{123, 456, 789, 1, 2, 3};
+   std::set<int> x{123, 456, 789, 1, 2, 3};
+   for (auto n: rsn::lib::make_slice_ref(x).drop_head().drop_tail_ex(2)) std:printf("%d\n", n);
+
+   return {};
+}
--- rusini.hh
+++ rusini.hh
@@ -23,22 +23,15 @@
 
 namespace rsn::lib {
 
-   namespace aux {
-      using std::begin, std::end;
-      template<typename Obj> using iterator_begin = decltype(begin(*static_cast<Obj *>(nullptr)));
-      template<typename Obj> using iterator_end = decltype(end(*static_cast<Obj *>(nullptr)));
-   }
-
-   template<typename Obj>
+   template<typename Iter>
    class slice_ref { // non-owning "reference" to a range of container elements (generalized analog of llvm::ArrayRef)
-   public: // expose typedef traits and check begin/end consistency
-      typedef aux::iterator_begin<Obj>                                 iterator, const_iterator;
+   public: // typedefs to imitate standard containers
+      typedef Iter                                                     iterator, const_iterator;
       typedef typename std::iterator_traits<iterator>::value_type      value_type;
       typedef typename std::iterator_traits<iterator>::pointer         pointer, const_pointer;
       typedef typename std::iterator_traits<iterator>::reference       reference, const_reference;
       typedef typename std::iterator_traits<iterator>::difference_type difference_type;
       typedef std::make_unsigned_t<difference_type>                    size_type;
-      static_assert (std::is_same_v<aux::iterator_end<Obj>, iterator>);
    public: // standard operations
       slice_ref() = default;
       /* implicitly defaulted copy and move constructors and assignment operators */
@@ -46,13 +39,12 @@ namespace rsn::lib {
       template<typename Lhs, typename Rhs> friend bool operator==(const slice_ref<Lhs> &, const slice_ref<Rhs> &) noexcept;
       template<typename Lhs, typename Rhs> friend bool operator!=(const slice_ref<Lhs> &, const slice_ref<Rhs> &) noexcept;
    public: // miscellaneous constructors and assignment operators
-      RSN_INLINE slice_ref(Obj &rhs) noexcept: _begin(std::begin(rhs)), _end(std::end(rhs)) {}
       RSN_INLINE slice_ref(iterator _begin, iterator _end) noexcept: _begin(std::move(_begin)), _end(std::move(_end)) {}
       template<typename Begin, typename End> RSN_INLINE slice_ref(Begin _begin, End _end) noexcept: _begin(std::move(_begin)), _end(std::move(_end)) {}
    public:
-      template<typename Rhs> RSN_INLINE slice_ref(Rhs &rhs) noexcept: _begin(std::begin(rhs)), _end(std::end(rhs)) {}
+      template<typename Rhs> RSN_INLINE slice_ref(Rhs &rhs) noexcept: slice_ref{adl_begin(rhs), adl_end(rhs)} {}
       template<typename Rhs> slice_ref(Rhs &&) = delete;
-      template<typename Rhs> RSN_INLINE slice_ref &operator=(Rhs &rhs) noexcept { _begin = std::begin(rhs), _end = std::end(rhs); return *this; }
+      template<typename Rhs> RSN_INLINE slice_ref &operator=(Rhs &rhs) noexcept { _begin = adl_begin(rhs), _end = adl_end(rhs); return *this; }
       template<typename Rhs> slice_ref &operator=(Rhs &&) = delete;
    public:
       template<typename Rhs> RSN_INLINE slice_ref(slice_ref<Rhs> &rhs) noexcept
@@ -71,39 +63,41 @@ namespace rsn::lib {
          { _begin = std::move(rhs._begin), _end = std::move(rhs._end); return *this; }
       template<typename Rhs> RSN_INLINE slice_ref &operator=(const slice_ref<Rhs> &&rhs) noexcept
          { _begin = rhs._begin, _end = rhs._end; return *this; }
-   public: // container-like operations
+   public: // container-like access/iteration
       RSN_INLINE bool empty() const noexcept { return end() == begin(); }
       RSN_INLINE size_type size() const noexcept { return end() - begin(); }
       RSN_INLINE size_type size_ex() const noexcept { return std::distance(begin(), end()); }
    public:
-      RSN_INLINE auto &begin() noexcept       { return _begin; }
-      RSN_INLINE auto &begin() const noexcept { return _begin; }
-      RSN_INLINE auto &end() noexcept       { return _end; }
-      RSN_INLINE auto &end() const noexcept { return _end; }
-      RSN_INLINE auto &cbegin() noexcept       { return begin(); }
-      RSN_INLINE auto &cbegin() const noexcept { return begin(); }
-      RSN_INLINE auto &cend() noexcept       { return end(); }
-      RSN_INLINE auto &cend() const noexcept { return end(); }
+      RSN_INLINE auto begin() const noexcept { return _begin; }
+      RSN_INLINE auto end() const noexcept { return _end; }
+      RSN_INLINE auto cbegin() const noexcept { return begin(); }
+      RSN_INLINE auto cend() const noexcept { return end(); }
    public: // convenience operations
-      RSN_INLINE value_type &head() const noexcept { return *begin(); }
-      RSN_INLINE value_type &tail() const noexcept { return *std::prev(end()); }
-      RSN_INLINE slice_ref skip_head() const noexcept { return {std::next(begin()), end()}; }
-      RSN_INLINE slice_ref skip_tail() const noexcept { return {begin(), std::prev(end())}; }
-      RSN_INLINE slice_ref skip_head(difference_type count) const noexcept { return {begin() + count, end()}; }
-      RSN_INLINE slice_ref skip_tail(difference_type count) const noexcept { return {begin(), end() - count}; }
-      RSN_INLINE slice_ref skip_head_ex(difference_type count) const noexcept { return {std::next(begin(), count), end()}; }
-      RSN_INLINE slice_ref skip_tail_ex(difference_type count) const noexcept { return {begin(), std::prev(end(), count)}; }
+      RSN_INLINE auto &head() const noexcept { return *begin(); }
+      RSN_INLINE auto &tail() const noexcept { return *std::prev(end()); }
+      RSN_INLINE slice_ref drop_head() const noexcept { return drop_head_ex(1); }
+      RSN_INLINE slice_ref drop_tail() const noexcept { return drop_tail_ex(1); }
+      RSN_INLINE slice_ref drop_head(difference_type count) const noexcept { return {begin() + count, end()}; }
+      RSN_INLINE slice_ref drop_tail(difference_type count) const noexcept { return {begin(), end() - count}; }
+      RSN_INLINE slice_ref drop_head_ex(difference_type count) const noexcept { return {std::next(begin(), count), end()}; }
+      RSN_INLINE slice_ref drop_tail_ex(difference_type count) const noexcept { return {begin(), std::prev(end(), count)}; }
    private: // internal representation
       iterator _begin, _end;
       template<typename> friend class slice_ref;
+   private: // implementation helpers
+      template<typename Cont> RSN_INLINE static auto adl_begin(Cont &&cont) { using std::begin; return begin(std::forward<Cont>(cont)); }
+      template<typename Cont> RSN_INLINE static auto adl_end(Cont &&cont) { using std::end; return end(std::forward<Cont>(cont)); }
    };
-   template<typename Obj>
-   RSN_INLINE inline void swap(slice_ref<Obj> &lhs, slice_ref<Obj> &rhs)
+   // non-member functions
+   template<typename Iter> RSN_INLINE inline void swap(slice_ref<Iter> &lhs, slice_ref<Iter> &rhs)
       { lhs.swap(rhs); }
    template<typename Lhs, typename Rhs> RSN_INLINE inline bool operator==(const slice_ref<Lhs> &lhs, const slice_ref<Rhs> &rhs) noexcept
       { return lhs.begin == rhs.begin && lhs.end == rhs.end; }
    template<typename Lhs, typename Rhs> RSN_INLINE inline bool operator!=(const slice_ref<Lhs> &lhs, const slice_ref<Rhs> &rhs) noexcept
       { return lhs.begin != rhs.begin || lhs.end != rhs.end; }
+   // construction helper
+   template<typename Cont> RSN_INLINE inline auto make_slice_ref(Cont &cont) noexcept
+      { using std::begin, std::end; return slice_ref{begin(cont), end(cont)}; }
 
 }
 
