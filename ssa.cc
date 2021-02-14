503853eaa8a13b0429404f278f7bbecaf7b8b376
rusini <rusini@manool.org>
2021-02-14 11:29:32 -0500

    Update 110

--- ssa.cc
+++ ssa.cc
@@ -19,121 +19,170 @@ namespace rsn::opt { void transform_to_ssa(proc *); }
    - A Simple, Fast Dominance Algorithm by Keith D. Cooper, Timothy J. Harvey, and Ken Kennedy
 */
 void rsn::opt::transform_to_ssa(proc *pc) {
-   // Build Dominator Tree /////////////////////////////////////////////////////
-   {  static constexpr auto intersect = [](const bblock *lhs, const bblock *rhs) noexcept RSN_INLINE{ // helper
-         for (auto finger_lhs = lhs, finger_rhs = rhs; finger_lhs != finger_rhs;) {
-            for (; finger_lhs->aux.postord_num < finger_rhs->aux.postord_num; finger_lhs = finger_lhs->aux.idom);
-            for (; finger_rhs->aux.postord_num < finger_lhs->aux.postord_num; finger_rhs = finger_rhs->aux.idom);
+   std::size_t bb_count = 0, vr_count = 0;
+   // Numerate BBs and VRs /////////////////////////////////////////////////////////////////////////
+   for (auto bb = pc->head(); bb; bb = bb->next()) {
+      bb->sn = bb_count++;
+      for (auto in = bb->head(); in; in = in->next()) {
+         for (const auto &input: in->inputs()) if (is<vreg>(input)) as<vreg>(input)->sn = -1;
+         for (const auto &output: in->outputs()) output->sn = -1;
+      }
+   }
+   for (auto bb = pc->head(); bb; bb = bb->next()) for (auto in = bb->head(); in; in = in->next()) {
+      for (const auto &input: in->inputs()) if (is<vreg>(input) && RSN_UNLIKELY(as<vreg>(input)->sn == -1)) as<vreg>(input)->sn = vr_count++;
+      for (const auto &output: in->outputs()) if (RSN_UNLIKELY(output->sn == -1)) output->sn = vr_count++;
+   }
+
+   lib::small_vec<std::vector<bblock *>, 510> preds(bb_count), succs(bb_count);
+   // Build BB Predecessor and Successor Lists (using preorder DFS) ////////////////////////////////
+   preds.extend(bb_count), succs.extend(bb_count);
+   {  lib::small_vec<bool, 510> visited(bb_count); visited.extend(bb_count, false);
+      auto traverse = [&preds, &succs](auto traverse, bblock *bb) RSN_NOINLINE{
+         if (RSN_UNLIKELY(visited[bb->sn])) return;
+         visited[bb->sn] = true;
+         for (const auto &target: bb->rear()->targets())
+         if (preds[target->sn].empty() || RSN_LIKELY(preds[target->sn].back() != bb))
+            preds[target->sn].push_back(bb), succs[bb->sn].push_back(target);
+         for (auto succ: succs[bb->sn]) traverse(traverse, succ);
+      };
+      traverse(traverse, pc->head());
+   }
+
+   lib::small_vec<bblock *, 510> idom(bb_count);
+   // Build Dominator Tree /////////////////////////////////////////////////////////////////////////
+   idom.extend(bb_count);
+   {  lib::small_vec<bblock *, 510> postord(bb_count);
+      lib::small_vec<long, 510> postord_num(bb_count);
+      // determine postorder ordering and numbering for the CFG
+      postord.extend(bb_count);
+      {  static constexpr auto traverse = [](auto traverse, decltype(visited) &visited, bblock *bb,
+         decltype(postord) &postord, decltype(postord_num) &postord_num, decltype(num) &num) noexcept RSN_NOINLINE{
+            if (RSN_UNLIKELY(visited[bb->sn])) return;
+            visited[bb->sn] = true;
+            for (auto succ: succs[bb->sn]) traverse(traverse, visited, succ, postord, postord_num, num);
+            postord.push_back(bb), postord_num[bb->sn] = num++;
+         };
+         lib::small_vec<bool, 510> visited(bb_count); visited.extend(bb_count, false);
+         postord_num::value_type num = {}; traverse(traverse, visited, pc->head(), postord, postord_num, num);
+      }
+      // helper routine for dominator set intersection
+      const auto intersect = [&](const bblock *lhs, const bblock *rhs) noexcept RSN_INLINE{
+         auto finger_lhs = lhs, finger_rhs = rhs;
+         while (finger_lhs != finger_rhs) {
+            for (; postord_num[finger_lhs->sn] < postord_num[finger_rhs->sn]; finger_lhs = idom[finger_lhs->sn]);
+            for (; postord_num[finger_rhs->sn] < postord_num[finger_lhs->sn]; finger_rhs = idom[finger_rhs->sn]);
          }
          return finger_lhs;
       };
-      // determine postorder ordering and numbering for the CFG
-      lib::small_vec<bblock *, 510> postord(lib::size(pc));
-      for (auto bb = pc->head(); bb = bb->next, bb;) bb->aux.visited = false;
-      {  long num = {};
-         static constexpr auto traverse = [](auto traverse, block *bb, decltype(postord) &postord, decltype(num) &num) noexcept RSN_NOINLINE{
-            if (RSN_UNLIKELY(bb->aux.visited)) return;
-            bb->aux.visited = true;
-            for (auto &target: bb->rear()->targets()) traverse(traverse, target, postord, num);
-            postord.push_back(bb), bb->aux.postord_num = num++;
-         };
-         traverse(traverse, pc->head(), postord, num);
-      }
       // initial state
-      pc->head()->aux.idom = pc->head();
-      for (auto bb = pc->head(); bb = bb->next, bb;) bb->aux.idom = {};
+      idom[pc->head()->sn] = pc->head();
+      for (auto bb = pc->head(); bb = bb->next(), bb;) idom[bb->sn] = {};
       // state transition until a fixed point is reached
       for (;;) {
          bool changed = false;
          for (auto bb: lib::range_ref(postord).drop_first().reverse()) {
-            auto idom = bb->aux.preds.head();
-            for (auto pred: lib::range_ref(bb->aux.preds).drop_first())
-               if (RSN_LIKELY(pred->aux.idom)) idom = intersect(pred, idom);
-            if (bb->aux.idom != idom)
-               changed = (bb->aux.idom = idom, true);
+            auto temp = lib::range_ref(preds[bb->sn]).first();
+            for (auto pred: lib::range_ref(preds[bb->sn]).drop_first())
+               if (RSN_LIKELY(idom[pred->sn])) temp = intersect(pred, temp);
+            if (idom[pred->sn] != temp)
+               changed = (idom[pred->sn] = temp, true);
          }
          if (RSN_UNLIKELY(!changed)) return;
       }
    }
-   {  auto finally_dom_front_v =
-         lib::finally{[pc]() noexcept RSN_INLINE{ for (auto bb = pc->head(); bb; bb = bb->next()) bb->aux_dom_front_v.clear(); }};
-   // Compute Dominance Frontiers //////////////////////////////////////////////
-      {  auto finally_dom_front_s =
-            lib::finally{[pc]() noexcept RSN_INLINE{ for (auto bb = pc->head(); bb; bb = bb->next()) bb->aux_dom_front_s.clear(); }};
+   {  std::vector<std::vector<bblock *>, 510> dom_front(bb_count, {});
+   // Compute Dominance Frontiers //////////////////////////////////////////////////////////////////
+      {  lib::small_vec<bool, 16 * 1024 - 8> dom_front_m(bb_count * bb_count);
+         dom_front_s.extend(bb_count * bb_count, false);
          for (auto bb = pc->head(); bb; bb = bb->next())
-         if (RSN_UNLIKELY(bb->aux_preds.size() > 1))
-         for (auto runner: bb->aux_preds)
-         for (; runner != bb->aux_idom; runner = runner->aux_idom)
-            runner->aux_dom_front_s.insert(bb);
-         for (auto bb = pc->head(); bb; bb = bb->next()) bb->aux_dom_front_v.clear(bb->aux_dom_front_s);
+         if (RSN_UNLIKELY(preds[bb->sn].size() > 1))
+         for (auto runner: preds[bb->sn])
+         for (; runner != idom[bb->sn]; runner = idom[runner->sn])
+         if (RSN_UNLIKELY(!dom_front_m[bb_count * runner->sn + bb->sn]))
+            dom_front_m[bb_count * runner->sn + bb->sn] = true, dom_front[runner->sn].push_back(bb);
       }
-   // Place Trivial Phi-functions for a Minimal (Non-pruned) SSA ///////////////
-      std::set<std::pair<vreg *, bblock *>> placed;
-      static constexpr auto traverse =
-      [](auto traverse, decltype(placed) &placed, vreg *vr, bblock *bb) RSN_NOINLINE{
-         for (auto _bb: bb->aux.dom_front_v) if (RSN_UNLIKELY(placed.find({vr, _bb}) == placed.end())) {
-            insn_phi::make(_bb->head(), std::vector<lib::smart_ptr<vreg>>(_bb->aux.preds.size(), vr), vr), placed.insert({vr, _bb});
-            traverse(traverse, placed, vr, _bb);
+   // Place Trivial Phi-functions for a Minimal (Non-pruned) SSA ///////////////////////////////////
+      std::vector<std::vector<bool>> placed(bb_count, std::vector<bool>(vr_count));
+      const auto place = [&](auto place, bblock *bb, vreg *vr) RSN_NOINLINE{
+         for (auto _bb: dom_front[bb->sn]) if (RSN_UNLIKELY(placed[bb->sn][vr->sn])) {
+            insn_phi::make(_bb->head(), std::vector<lib::smart_ptr<vreg>>(preds[_bb->sn].size(), vr), vr), placed[bb->sn][vr->sn] = true;
+            place(place, _bb, vr);
          }
       };
-      // O(statements * blocks * log(statements * blocks)), which is probably OK:
+      for (auto bb = pc->head(); bb; bb = bb->next()) for (auto in = bb->head(); in; in = in->next())
+         if (RSN_UNLIKELY(!is<insn_phi>(in))) for (const auto &output: in->outputs()) place(place, bb, output);
+
+      lib::small_vec<bool, 16 * 1024 - 8> placed(bb_count * vr_count);
+      const auto traverse = [&](auto traverse, vreg *vr, bblock *bb) RSN_NOINLINE{
+         for (auto _bb: dom_front[bb->sn]) if (RSN_UNLIKELY(placed[vr_count * bb->sn + vr->sn])) {
+            insn_phi::make(_bb->head(), std::vector<lib::smart_ptr<vreg>>(preds[_bb->sn].size(), vr), vr),
+               placed[vr_count * bb->sn + vr->sn] = true;
+            traverse(traverse, vr, _bb);
+         }
+      };
+      placed.extend(bb_count * vr_count, false);
       for (auto bb = pc->head(); bb; bb = bb->next()) for (auto in = bb->head(); in; in = in->next())
          if (RSN_LIKELY(!is<insn_phi>(in))) for (auto &output: in->outputs()) traverse(traverse, placed, output, bb);
    }
-   // Rename Virtual Registers /////////////////////////////////////////////////
-   {  static constexpr auto traverse =
-      [](auto traverse, bblock *bb) RSN_NOINLINE{
-         if (RSN_UNLKELY(bb->aux.visited)) return;
-         bb->aux.visited = true;
-         // stack to restore mapping at the end
+   // Rename Virtual Registers /////////////////////////////////////////////////////////////////////
+   {  lib::small_vec<vreg *, 510> vr_map(vr_count);
+      lib::small_vec<std::size_t, 510> phi_arg_index(bb_count);
+      lib::small_vec<bool, 510> visited(bb_count);
+      static constexpr auto traverse = [](auto traverse, decltype(visited) &visited, bblock *bb,
+      decltype(vr_map) &vr_map, decltype(phi_arg_index) &phi_arg_index) RSN_NOINLINE{
+         if (RSN_UNLKELY(visited[bb->sn])) return;
+         visited[bb->sn] = true;
+         // stack to restore VR mapping at the end
          lib::small_vec<std::pair<lib::smart_ptr<vreg>, vreg *>> stack(
          [bb]() noexcept RSN_INLINE{
             lib::small_vec<std::pair<lib::smart_ptr<vreg>, vreg *>>::size_type count = 0;
-            auto in = bb->head();
-            for (; is<insn_phi>(in); in = in->next()) {
-               ++count;
-            }
-            for (; in; in = in->next()) {
-               for (auto &input: in->inputs()) if (is<vreg>(input)) ++count;
-               count += in->outputs().count();
+            {  auto in = bb->head();
+               for (; is<insn_phi>(in); in = in->next()) {
+                  ++count;
+               }
+               for (; in; in = in->next()) {
+                  for (auto &input: in->inputs()) if (is<vreg>(input)) ++count;
+                  count += in->outputs().count();
+               }
             }
             return count;
          }() );
          auto in = bb->head();
          // rewrite phi destinations
          for (; is<insn_phi>(in); in = in->next()) {
-            stack.push_back({std::move(as<insn_phi>(in)->dest()), as<insn_phi>(in)->dest()->aux.vr}),
-               as<insn_phi>(in)->dest() = vreg::make(), stack.back().first->aux.vr = as<insn_phi>(in)->dest();
+            stack.push_back({std::move(as<insn_phi>(in)->dest()), vr_map[as<insn_phi>(in)->dest()->sn]}),
+               as<insn_phi>(in)->dest() = vreg::make(), vr_map[stack.back().first->sn] = as<insn_phi>(in)->dest();
          }
          // rewrite normal instructions
          for (; in; in = in->next()) {
             for (auto &input: in->inputs())
-               if (is<vreg>(input)) input = as<vreg>(input)->aux.vr;
+               if (is<vreg>(input)) input = vr_map[as<vreg>(input)->sn];
             for (auto &output: in->outputs())
-               stack.push_back({std::move(output), output->aux.vr}),
-                  output = vreg::make(), stack.back().first->aux.vr = output;
+               stack.push_back({std::move(output), vr_map[output->sn]}),
+                  output = vreg::make(), vr_map[stack.back().first->sn] = output;
          }
          // process successors
-         for (auto _bb: bb->aux.succs) {
+         for (auto _bb: succs[bb->sn]) {
             // rewrite phi inputs
             for (auto in = _bb->head(); is<insn_phi>(in); in = in->next())
-               as<insn_phi>(in)->args()[_bb->aux.phi_arg_index] = as<insn_phi>(in)->args()[_bb->aux.phi_arg_index]->aux.vr;
-            ++_bb->aux.phi_arg_index;
+               as<insn_phi>(in)->args()[phi_arg_index[_bb->sn]] = vr_map[as<insn_phi>(in)->args()[phi_arg_index[_bb->sn]]->sn];
+            ++phi_arg_index[_bb->sn];
             // recur into the successor
-            traverse(traverse, _bb);
+            traverse(traverse, visited, _bb, vr_map, phi_arg_index);
          }
-         // restore mapping
+         // restore VR mapping
          while (!stack.empty())
-            stack.back().first->aux.vr = stack.back().second, stack.pop_back();
+            vr_map[stack.back().first->sn] = stack.back().second, stack.pop_back();
       };
       // initialization and start
+      vr_map.extend(vr_count);
       for (auto bb = pc->head(); bb; bb = bb->next()) {
-         bb->aux.visited = false, bb->aux.phi_arg_index = 0;
          for (auto in = bb->head(); in; in = in->next()) {
-            for (auto &input: in->inputs()) if (is<vreg>(input)) as<vreg>(input)->aux.vr = as<vreg>(input);
-            for (auto &output: in->outputs()) output->aux.vr = output;
+            for (const auto &input: in->inputs()) if (is<vreg>(input)) vr_map[as<vreg>(input)->sn] = as<vreg>(input);
+            for (const auto &output: in->outputs()) vr_map[output->sn] = output;
          }
       }
-      traverse(traverse, pc->head());
+      phi_arg_index.extend(bb_count, 0);
+      traverse(traverse, (visited.extend(bb_count, false), visited), pc->head(), vr_map, phi_arg_index);
    }
 }
