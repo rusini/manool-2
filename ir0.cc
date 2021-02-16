a586f36e9d04a2009a2e15144cc0ea468d1bbc20
rusini <rusini@manool.org>
2021-02-15 19:21:41 -0500

    Update 116

--- ir0.cc
+++ ir0.cc
@@ -17,23 +17,22 @@
 
 # include <unordered_set>
 
-using namespace rsn::opt;
-
-void proc::dump() const noexcept {
+void rsn::opt::proc::dump() const noexcept {
    std::fprintf(stderr, "P%u = proc $0x%08X[0x%016llX%016llX] as\n", node::sn, (unsigned)id.first, id.second, id.first);
    {  std::unordered_set<bblock *> bblocks;
-      for (auto bb: all(this)) for (auto in: all(bb)) {
-         for (auto target: in->targets()) if (target->owner() != this) bblocks.insert(target);
+      for (auto bb = head(); bb; bb = bb->next()) for (auto in = bb->head(); in; in = in->next()) {
+         for (auto &target: in->targets()) if (target->owner() != this) bblocks.insert(target);
       }
       for (auto it: bblocks) log << "  ; Error: Reference to foreign bblock " << it << '\n';
    }
-   for (auto it: all(this)) it->dump();
+   for (auto bb = head(); bb; bb = bb->next()) bb->dump();
    std::fprintf(stderr, "end proc P%u\n\n", node::sn);
 }
 
-void bblock::dump() const noexcept {
+void rsn::opt::bblock::dump() const noexcept {
    std::fprintf(stderr, "L%u:\n", node::sn);
-   for (auto insn: all(this)) std::fputs("    ", stderr), insn->dump(), std::fputc('\n', stderr);
+   for (auto in = head(); in; in = in->next())
+      std::fputs("    ", stderr), in->dump(), std::fputc('\n', stderr);
 }
 
 # endif // # if RSN_USE_DEBUG
